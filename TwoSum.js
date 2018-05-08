9db971d80ab139fdd083310079e955f858f247b0
Justin Fangrad <justin.fangrad@gmail.com>
2018-05-08 16:35:02 -0400

    Two Sum Solution

---
+++ TwoSum.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const map = new Map();
+    for (var i = 0; i < nums.length; i++) {
+        const compliment = target - nums[i];
+        if (map.has(compliment)) {
+            return [map.get(compliment), i];
+        }
+        map.set(nums[i], i);
+    }
+};
